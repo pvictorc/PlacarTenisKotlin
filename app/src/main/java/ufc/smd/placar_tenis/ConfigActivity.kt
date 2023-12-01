26b83c066b7ab1dc286d38cb8dfd9c4a50b92be6
pvictorc <pvictorc@gmail.com>
2023-12-01 17:21:30 -0300

    nome da partida

--- app/src/main/java/ufc/smd/placar_tenis/ConfigActivity.kt
+++ app/src/main/java/ufc/smd/placar_tenis/ConfigActivity.kt
@@ -3,16 +3,25 @@ package ufc.smd.placar_tenis
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Switch
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import data.Placar
 import ufc.smd.placar_tenis.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ConfigActivity : AppCompatActivity() {
-    var placar: Placar= Placar("Jogo sem Config","0x0", "20/05/20 10h",false)
+    val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")
+    val current = LocalDateTime.now().format(formatter)
+    var placar: Placar= Placar("Jogo sem Config","0x0", current,false)
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
@@ -20,6 +29,7 @@ class ConfigActivity : AppCompatActivity() {
         //Log.v("PDM22",placar.nome_partida)
         //Log.v("PDM22",placar.has_timer.toString())
 
+//        val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")
 
         openConfig()
         initInterface()
@@ -30,21 +40,27 @@ class ConfigActivity : AppCompatActivity() {
         val sp:SharedPreferences = getSharedPreferences(sharedFilename,Context.MODE_PRIVATE)
         var edShared = sp.edit()
 
-
         edShared.putString("matchname",placar.nome_partida)
         edShared.putBoolean("has_timer",placar.has_timer)
         edShared.commit()
     }
-    fun openConfig()
-    {
+    fun openConfig() {
         val sharedFilename = "configPlacar"
         val sp:SharedPreferences = getSharedPreferences(sharedFilename,Context.MODE_PRIVATE)
+        val nome_jogo = "Partida em "+LocalDateTime.now().toLocalDate().toString()
+
         placar.nome_partida=sp.getString("matchname","Jogo Padrão").toString()
         placar.has_timer=sp.getBoolean("has_timer",false)
 
+        Log.v("PDM23","openConf: "+placar.nome_partida)
     }
     fun initInterface(){
         val tv= findViewById<EditText>(R.id.editTextGameName)
+        val nome_jogo = "Partida em "+LocalDateTime.now().format(formatter).toString()
+        var txtNomeJogo = findViewById<TextView>(R.id.editTextGameName)
+        txtNomeJogo.text = nome_jogo
+        placar.nome_partida = nome_jogo.toString()
+        Log.v("PDM23",placar.nome_partida)
         tv.setText(placar.nome_partida)
         val sw= findViewById<Switch>(R.id.swTimer)
         sw.isChecked=placar.has_timer
